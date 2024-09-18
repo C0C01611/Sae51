71bd2ad3058671356208866103e127ee117361af
C0C01611 <160739032+C0C01611@users.noreply.github.com>
2024-09-18 13:16:42 +0200

    Update genMV.sh

--- genMV.sh
+++ genMV.sh
@@ -1,90 +1,62 @@
 #!/bin/bash
 
-
 # Variables
-
 RAM=4096
-
 DISK=64000
 
-VM_NAME="Debian1"
+# Demander à l'utilisateur de spécifier le nombre de VM à créer
+read -p "Entrez le nombre de VM : " num_vms
 
-OS_TYPE="Debian_64"
+# Préfixe du nom des VM
+prefix="Debian"
 
-NIC_TYPE="nat"
+# Boucle pour créer les VM
+for i in $(seq 1 $num_vms); do
+    VM_NAME="${prefix}${i}"
 
+    OS_TYPE="Debian_64"
+    NIC_TYPE="nat"
 
-# Fonctions
+    # Fonctions
 
-create_vm() {
+    create_vm() {
+        echo "Creating VM: $VM_NAME"
+        VBoxManage createvm --name $VM_NAME --ostype $OS_TYPE --register
+        VBoxManage modifyvm $VM_NAME --memory $RAM --nic1 $NIC_TYPE --boot1 net
+        VBoxManage createhd --filename "$VM_NAME.vdi" --size $DISK
+        VBoxManage storagectl $VM_NAME --name "SATA Controller" --add sata --controller IntelAhci
+        VBoxManage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_NAME.vdi"
+        echo "VM $VM_NAME created successfully!"
+    }
 
-    echo "Creating VM: $VM_NAME"
+    delete_vm() {
+        echo "Deleting VM: $VM_NAME"
+        VBoxManage unregistervm $VM_NAME --delete
+        echo "VM $VM_NAME deleted successfully!"
+    }
 
-    VBoxManage createvm --name $VM_NAME --ostype $OS_TYPE --register
+    list_vms() {
+        VBoxManage list vms
+    }
 
-    VBoxManage modifyvm $VM_NAME --memory $RAM --nic1 $NIC_TYPE --boot1 net
+    start_vm() {
+        echo "Starting VM: $VM_NAME"
+        VBoxManage startvm $VM_NAME --type headless
+    }
 
-    VBoxManage createhd --filename "$VM_NAME.vdi" --size $DISK
+    stop_vm() {
+        echo "Stopping VM: $VM_NAME"
+        VBoxManage controlvm $VM_NAME acpipowerbutton
+    }
 
-    VBoxManage storagectl $VM_NAME --name "SATA Controller" --add sata --controller IntelAhci
+    # Main Script
+    case $1 in
+        N) create_vm ;;
+        S) delete_vm ;;
+        L) list_vms ;;
+        D) start_vm ;;
+        A) stop_vm ;;
+        *) echo "Invalid option. Use N (New), S (Delete), L (List), D (Start), A (Stop)" ;;
+    esac
 
-    VBoxManage storageattach $VM_NAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VM_NAME.vdi"
-
-    echo "VM $VM_NAME created successfully!"
-
-}
-
-
-delete_vm() {
-
-    echo "Deleting VM: $VM_NAME"
-
-    VBoxManage unregistervm $VM_NAME --delete
-
-    echo "VM $VM_NAME deleted successfully!"
-
-}
-
-
-list_vms() {
-
-    VBoxManage list vms
-
-}
-
-
-start_vm() {
-
-    echo "Starting VM: $VM_NAME"
-
-    VBoxManage startvm $VM_NAME --type headless
-
-}
-
-
-stop_vm() {
-
-    echo "Stopping VM: $VM_NAME"
-
-    VBoxManage controlvm $VM_NAME acpipowerbutton
-
-}
-
-
-# Main Script
-
-case $1 in
-
-    N) create_vm ;;
-
-    S) delete_vm ;;
-
-    L) list_vms ;;
-
-    D) start_vm ;;
-
-    A) stop_vm ;;
-
-    *) echo "Invalid option. Use N (New), S (Delete), L (List), D (Start), A (Stop)" ;;
-
-esac
+done
